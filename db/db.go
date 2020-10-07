24ea30f125f343c96e7b515a8e29842c2ff2a3cb
Leo Takaoka <le0tk0k@gmail.com>
2020-10-07 22:27:02 +0900

    add: add db/db.go

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/Le0tk0k/peingo/config"
+	"github.com/jmoiron/sqlx"
+)
+
+func NewDb() (*sqlx.DB, error) {
+	db, err := sqlx.Open("mysql", config.DSN())
+	if err != nil {
+		return nil, fmt.Errorf("failed to Open mysql: %v", err)
+	}
+
+	// 接続確認
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping: %v", err)
+	}
+
+	return db, nil
+}
