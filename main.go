029ebb9205d482a7ee2c404a9a313226853b2b86
Leo Takaoka <le0tk0k@gmail.com>
2020-10-09 08:13:00 +0900

    update main

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"github.com/Le0tk0k/peingo/router"
+	"log"
+
+	"github.com/Le0tk0k/peingo/db"
+	"github.com/Le0tk0k/peingo/usecase"
+	"github.com/Le0tk0k/peingo/web/router"
 )
 
 func main() {
-	router.Init()
+	conn, err := db.NewDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	qnaRepo := db.NewQnARepository(conn)
+	qnaUC := usecase.NewQnAUseCase(qnaRepo)
+	r := router.Router(qnaUC)
+
+	r.Logger.Fatal(r.Start(":1323"))
 }
