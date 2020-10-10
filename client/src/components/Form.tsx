477ddcf1e8b3ac4adaae26f571e94828c5b8c935
Leo Takaoka <le0tk0k@gmail.com>
2020-10-10 23:55:26 +0900

    add: add Form

---
+++ client/src/components/Form.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const Form: FC = () => (
+    <div>
+        <p>気軽にどうぞ！</p>
+        <textarea></textarea>
+        <button>質問する</button>
+    </div>
+);
+
+export default Form;
