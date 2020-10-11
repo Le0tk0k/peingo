ae346ec099da6b31806eba3507ff9109553e5d23
Leo Takaoka <le0tk0k@gmail.com>
2020-10-11 23:12:33 +0900

    style: change css to scss in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 import Index from './pages/Index';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Index />
     </div>
   );
