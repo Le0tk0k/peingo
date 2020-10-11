e2e3f8608b6904ab812eb8d14c922a8867c24b08
Leo Takaoka <le0tk0k@gmail.com>
2020-10-11 11:30:45 +0900

    chore: apply prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,7 +12,12 @@ module.exports = {
         'plugin:import/typescript',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+        'prettier/standard'
     ],
     globals: {
         Atomics: 'readonly',
@@ -32,6 +37,7 @@ module.exports = {
         '@typescript-eslint',
         'import',
         'jsx-a11y',
+        'prettier',
         'react',
         'react-hooks',
 
