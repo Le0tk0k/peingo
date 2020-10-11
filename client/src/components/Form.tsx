aa10e9d33470fd38f1be4e9a58c7b84470837544
Leo Takaoka <le0tk0k@gmail.com>
2020-10-11 16:10:58 +0900

    style: add Form style

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
+import styles from './Form.module.scss';
 
 const Form: FC = () => (
-  <div>
+  <div className={styles.form}>
     <p>気軽にどうぞ！</p>
     <textarea />
     <button>質問する</button>
