69191b65d5ef526e1131fe3a660ff61530ad09a4
Leo Takaoka <le0tk0k@gmail.com>
2020-10-11 23:03:52 +0900

    add: add Questions

---
+++ client/src/components/Questions.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+
+interface Question {
+  id: number;
+  body: string;
+}
+
+const Questions: FC = () => {
+  const questions: Question[] = [
+    {
+      id: 0,
+      body: 'this is a question1.',
+    },
+    {
+      id: 2,
+      body: 'this is a question2.',
+    },
+  ];
+
+  return (
+    <ul>
+      {questions.map((question) => (
+        <li>{question.body}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Questions;
--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Form from '../components/Form';
+import Questions from '../components/Questions';
 
 const Index: FC = () => (
   <>
     <Header />
     <Hero />
     <Form />
+    <Questions />
   </>
 );
 export default Index;
