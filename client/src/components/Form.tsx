b51a93469327866c8e041cdd63b4724545fe5fe8
Leo Takaoka <le0tk0k@gmail.com>
2020-10-12 08:57:18 +0900

    update: update Form

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,12 +1,30 @@
 import React, { FC } from 'react';
+import useState from 'react';
 import styles from './Form.module.scss';
 
-const Form: FC = () => (
-  <div className={styles.form}>
-    <p>気軽にどうぞ！</p>
-    <textarea />
-    <button>質問する</button>
-  </div>
-);
+type Props = {
+  onHandleSubmit: (question: string) => void;
+};
+
+const Form: FC<Props> = (onHandleSubmit) => {
+  const [question, setQuestion] = useState('');
+
+  const handleChange = (e: any) => {
+    setQuestion(e.target.value);
+  };
+
+  const handleClick = () => {
+    onHandleSubmit(question);
+    setQuestion('');
+  };
+
+  return (
+    <div className={styles.form}>
+      <p>気軽にどうぞ！</p>
+      <textarea value={question} onChange={handleChange} />
+      <button onClick={handleClick}>質問する</button>
+    </div>
+  );
+};
 
 export default Form;
--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -4,11 +4,13 @@ import Hero from '../components/Hero';
 import Form from '../components/Form';
 import Questions from '../components/Questions';
 
+const handleSubmit = () => {};
+
 const Index: FC = () => (
   <>
     <Header />
     <Hero />
-    <Form />
+    <Form onHandleSubmit={handleSubmit} />
     <Questions />
   </>
 );
