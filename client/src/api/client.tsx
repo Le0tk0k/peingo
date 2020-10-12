033ca5f62175c079e702c1729adf2af9f1f93f19
Leo Takaoka <le0tk0k@gmail.com>
2020-10-12 10:19:27 +0900

    add: add post request

---
+++ client/src/api/client.tsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:1323/';
+
+const instance = axios.create({
+  baseURL,
+});
+
+export const postQuestion = async (req: string) => {
+  return await instance.post(`qnas`, req);
+};
