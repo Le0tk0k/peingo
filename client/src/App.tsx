62ac8e1886e54098c87d571ed0f0bc5d2cce4e49
Leo Takaoka <le0tk0k@gmail.com>
2020-10-14 08:59:56 +0900

    add: add QnA page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
+import { Switch, Route } from 'react-router';
 import styles from './App.module.scss';
 import Index from './pages/Index';
+import QnA from './pages/QnA';
 
 const App: FC = () => (
   <div className={styles.App}>
-    <Index />
+    <Switch>
+      <Route exact path="/">
+        <Index />
+      </Route>
+      <Route path="/:id">
+        <QnA />
+      </Route>
+    </Switch>
   </div>
 );
-
 export default App;
---
+++ client/src/pages/QnA.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import Header from '../components/Header';
+
+const QnA: FC = () => (
+  <>
+    <Header />
+  </>
+);
+export default QnA;
