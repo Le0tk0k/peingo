2780a7ba48e01dd02482c762e4dc5e29ff99599a
Leo Takaoka <le0tk0k@gmail.com>
2021-01-17 15:13:39 +0900

    update: 質問機能を実装

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,12 +1,27 @@
 import React, { FC, useState } from 'react';
 import styles from './Form.module.scss';
 
-const Form: FC = () => {
+type Props = {
+    onHandleSubmit: (question: string) => void;
+};
+
+const Form: FC<Props> = (props: Props) => {
+    const [question, setQuestion] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setQuestion(e.target.value);
+    };
+
+    const handleClick = () => {
+        props.onHandleSubmit(question);
+        setQuestion('');
+    };
+
     return (
         <div className={styles.form}>
             <p>気軽にどうぞ！</p>
-            <textarea />
-            <button>
+            <textarea value={question} onChange={handleChange} />
+            <button onClick={handleClick} disabled={!question}>
                 質問する
             </button>
         </div>
