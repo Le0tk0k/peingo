b21d5c56d86139d52d753f0b84db5065d3e52d4a
Leo Takaoka <le0tk0k@gmail.com>
2021-01-22 23:37:08 +0900

    update: wrap error in usecase/qna.go

--- usecase/qna.go
+++ usecase/qna.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"fmt"
+
 	"github.com/Le0tk0k/peingo/domain/entity"
 	"github.com/Le0tk0k/peingo/domain/repository"
 )
@@ -24,7 +26,7 @@ func NewQnAUseCase(r repository.QnARepository) QnAUseCase {
 func (u *qnaUseCase) GetQnA(id int) (*entity.QnA, error) {
 	qna, err := u.qnaRepository.FindByID(id)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("GetQnA id = %d : %w", id, err)
 	}
 	return qna, nil
 }
@@ -32,7 +34,7 @@ func (u *qnaUseCase) GetQnA(id int) (*entity.QnA, error) {
 func (u *qnaUseCase) GetQnAs() ([]*entity.QnA, error) {
 	qnas, err := u.qnaRepository.FindQnAs()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("GetQnAs : %w", err)
 	}
 	return qnas, err
 }
@@ -40,7 +42,7 @@ func (u *qnaUseCase) GetQnAs() ([]*entity.QnA, error) {
 func (u *qnaUseCase) GetAllQnAs() ([]*entity.QnA, error) {
 	qnas, err := u.qnaRepository.FindAllQnAs()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("GetAllQnAs : %w", err)
 	}
 	return qnas, err
 }
@@ -48,7 +50,7 @@ func (u *qnaUseCase) GetAllQnAs() ([]*entity.QnA, error) {
 func (u *qnaUseCase) CreateQuestion(qna *entity.QnA) error {
 	err := u.qnaRepository.StoreQuestion(qna)
 	if err != nil {
-		return err
+		return fmt.Errorf("CreateQuestion : %w", err)
 	}
 	return nil
 }
@@ -56,7 +58,7 @@ func (u *qnaUseCase) CreateQuestion(qna *entity.QnA) error {
 func (u *qnaUseCase) CreateAnswer(qna *entity.QnA) error {
 	err := u.qnaRepository.StoreAnswer(qna)
 	if err != nil {
-		return err
+		return fmt.Errorf("CreateAnswer : %w", err)
 	}
 	return nil
 }
